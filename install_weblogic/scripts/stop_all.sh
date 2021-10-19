a4108c821996d43a8ad4bb8224344868fe446ac6
KanyapatC <56994838+KanyapatC@users.noreply.github.com>
2021-10-19 15:40:16 +0700

    Update stop_all.sh
    แก้ชื่อ hostname

--- install_weblogic/scripts/stop_all.sh
+++ install_weblogic/scripts/stop_all.sh
@@ -5,7 +5,7 @@
 
 # Define Stop steps
 
-  nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/stopManagedWebLogic.sh Server1 t3://chubblife-icom-app.motif.corp:7001 > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Server1.out 2>&1 &
+  nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/stopManagedWebLogic.sh Server1 t3://<Your hostname or ip address>:7001 > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Server1.out 2>&1 &
   sleep 10
   nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/stopWebLogic.sh > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Admin.out 2>&1 &
 
