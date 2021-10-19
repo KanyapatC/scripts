50560e1fe2d90972969afd1ec973427b4a511624
KanyapatC <56994838+KanyapatC@users.noreply.github.com>
2021-10-19 15:38:38 +0700

    Update start_all.sh
    แก้ชื่อ hostname

--- install_weblogic/scripts/start_all.sh
+++ install_weblogic/scripts/start_all.sh
@@ -7,5 +7,5 @@
   nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/startWebLogic.sh > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Admin.out 2>&1 &
   sleep 10
 
-  nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/startManagedWebLogic.sh Server1 t3://chubblife-icom-app.motif.corp:7001 > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Server1.out 2>&1 &
+  nohup /u01/oracle/middleware/wls12214/user_projects/domains/base_domain/bin/startManagedWebLogic.sh Server1 t3://<Your hostname or ip address>:7001 > /u01/oracle/wls12214/user_projects/domains/base_domain/bin/nohup-Server1.out 2>&1 &
 
